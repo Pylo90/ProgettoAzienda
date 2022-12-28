a43df45587d43a1146cb14f19b89619c760a592f
Davide <Davide@DESKTOP-Q1FBO9P>
2022-12-28 20:38:01 +0100

    Aggiunta form di Samuele

--- src/main/java/application/ProgettoAzienda.java
+++ src/main/java/application/ProgettoAzienda.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package application;
 
-/**
- *
- * @author Davide
- */
 public class ProgettoAzienda {
-    
+    public static void main(String[] args) {
+        //inizializzazione login
+    }
 }
