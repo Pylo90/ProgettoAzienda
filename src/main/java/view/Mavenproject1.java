8efc882816c701bd93fa05bb28e27336efcf34e1
Davide <Davide@DESKTOP-Q1FBO9P>
2022-12-28 20:26:39 +0100

    Aggiunta form di Dario

---
+++ src/main/java/view/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package view;
+
+/**
+ *
+ * @author dario
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        new ScioperoForm().setVisible(true);
+    }
+}
