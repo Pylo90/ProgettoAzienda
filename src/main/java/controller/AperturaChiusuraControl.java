4d0ed2471acdb0510a9f12661f56335cf92b9df1
Davide <Davide@DESKTOP-Q1FBO9P>
2023-01-10 05:10:42 +0100

    Implementazione AperturaChiusuraControl

--- src/main/java/controller/AperturaChiusuraControl.java
+++ src/main/java/controller/AperturaChiusuraControl.java
@@ -1,8 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import misc.DBMSBoundary;
 
 /**
  *
@@ -10,13 +10,44 @@ package controller;
  */
 
 public class AperturaChiusuraControl {
-
+    
+    public static final int num_livelli = 5;
+    
     public AperturaChiusuraControl() {
         super();
     }
     
     public static void checkEmployees() {
+        int count_livelli[] = new int[num_livelli];
+        for(int i = 0; i < num_livelli; i++) {
+            count_livelli[i] = 0;
+        }
         
+        ResultSet rs = DBMSBoundary.getQuery("SELECT servizio_firmato FROM Impiegato WHERE servizio_firmato IS NOT NULL");
+        try {
+            while(rs.next()) {
+                count_livelli[rs.getInt("servizio_firmato") - 1]++;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+        rs = DBMSBoundary.getQuery("SELECT livello, minDipendenti FROM Servizio");
+        try {
+            while(rs.next()) {
+                if(count_livelli[rs.getInt("livello") - 1] < rs.getInt("minDipendenti")) {
+                    DBMSBoundary.updateQuery("UPDATE Servizio SET isOpen = 0 WHERE livello = " + rs.getInt("livello"));
+                } else {
+                    DBMSBoundary.updateQuery("UPDATE Servizio SET isOpen = 1 WHERE livello = " + rs.getInt("livello"));
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        AperturaChiusuraControl.checkEmployees();
     }
     
 }
