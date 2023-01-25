ed0494c9222118e5a2e6beaf7b77683d15094a71
Pylo90 <davide2000_09@hotmail.com>
2023-01-25 18:23:21 +0100

    Modifiche varie

--- src/main/java/misc/FirmaCheck.java
+++ src/main/java/misc/FirmaCheck.java
@@ -20,37 +20,38 @@ import model.Utente;
  * @author Davide
  */
 public class FirmaCheck implements Runnable {
-    
+
     private JComponent firmaButton;
-    
+
     public FirmaCheck(JComponent firmaButton) {
         this.firmaButton = firmaButton;
     }
-    
+
     @Override
-    public void run(){
+    public void run() {
         ResultSet rs;
-        while(true) {
+        while (true) {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(FirmaCheck.class.getName()).log(Level.SEVERE, null, ex);
             }
-            rs = DBMSBoundary.getQuery("SELECT MIN(ora) as firstHour FROM turno JOIN assegnazione_turno ON id = turno WHERE impiegato = '" + Utente.getMatricola() + "' AND data_ = '" + LocalDate.now().toString()+"';");
+            rs = DBMSBoundary.getQuery("SELECT MIN(ora) as firstHour FROM turno JOIN assegnazione_turno ON id = turno WHERE impiegato = '" + Utente.getMatricola() + "' AND data_ = '" + LocalDate.now().toString() + "';");
             try {
-                rs.next();
-                
-                if(rs.getInt("firstHour") != LocalTime.now().getHour()){
-                    firmaButton.setVisible(false);
-                    continue;
+                while (rs.next()) {
+
+                    if (rs.getInt("firstHour") != LocalTime.now().getHour()) {
+                        firmaButton.setVisible(false);
+                        continue;
+                    }
+
+                    if (LocalTime.now().getMinute() >= 10) {
+                        firmaButton.setVisible(false);
+                        continue;
+                    }
+
+                    firmaButton.setVisible(true);
                 }
-                
-                if(LocalTime.now().getMinute() >= 10) {
-                    firmaButton.setVisible(false);
-                    continue;
-                }
-                
-                firmaButton.setVisible(true);
             } catch (SQLException ex) {
                 Logger.getLogger(FirmaCheck.class.getName()).log(Level.SEVERE, null, ex);
             }
